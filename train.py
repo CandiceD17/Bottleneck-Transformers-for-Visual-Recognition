057e237b69d6c63d0cd40d54bdbf1cd1d96deb53
candiced <ruideng17@gmail.com>
2021-03-03 15:09:59 +0800

    build: debug

--- train.py
+++ train.py
@@ -11,39 +11,39 @@ EPOCHS = 5
 IMAGE_DIR = "./ImageNet/"
 
 def train(net, dataloader, optimizer, epoch, device):
-   print("            =======  Training  ======= \n")
-   net.train()
-   criterion = nn.CrossEntropyLoss()
-   
-   for ep in range(0, epoch):
-       train_loss = correct = total = 0
-       
-       for idx, (inputs, targets) in enumerate(train_loader):
-           inputs, targets = inputs.to(device), targets.to(device)
-           outputs = net(inputs)
-           
-           loss = criterion(outputs, targets)
-           optimizer.zero_grad()
-           loss.backward()
-           optimizer.step()
-           
-           train_loss += loss.item()
-           total += targets.size(0)
-           correct += torch.eq(outputs.argmax(dim=1), targets).sum().item()
-           
-           if ((idx + 1) % 20 == 0 or (idx + 1) == len(train_loader)):
-                print(
-                    "   == step: [{:3}/{}] [{}/{}] | loss: {:.3f} | acc: {:6.3f}%".format(
-                        idx + 1,
-                        len(train_loader),
-                        ep,
-                        epoch,
-                        train_loss / (idx + 1),
-                        100.0 * correct / total,
-                    )
-                )
-                
-     print("\n            =======  Training Finished  ======= \n")
+    print("            =======  Training  ======= \n")
+    net.train()
+    criterion = nn.CrossEntropyLoss()
+    
+    for ep in range(0, epoch):
+        train_loss = correct = total = 0
+        
+        for idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+
+            loss = criterion(outputs, targets)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            total += targets.size(0)
+            correct += torch.eq(outputs.argmax(dim=1), targets).sum().item()
+
+            if ((idx + 1) % 20 == 0 or (idx + 1) == len(train_loader)):
+                 print(
+                     "   == step: [{:3}/{}] [{}/{}] | loss: {:.3f} | acc: {:6.3f}%".format(
+                         idx + 1,
+                         len(train_loader),
+                         ep,
+                         epoch,
+                         train_loss / (idx + 1),
+                         100.0 * correct / total,
+                     )
+                 )
+    
+    print("\n            =======  Training Finished  ======= \n")
 
    
 if __name__ == "__main__":
